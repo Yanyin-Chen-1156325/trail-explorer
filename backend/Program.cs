73759a5cbf8edf36b919ffd8199cbaed21681d62
Yanyin-Chen-1156325 <yan-yin.chen@lincolnuni.ac.nz>
2026-06-24 19:06:54 +1200

    create db

--- backend/Program.cs
+++ backend/Program.cs
@@ -55,6 +55,14 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext =
+        scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
